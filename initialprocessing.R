8442fd2f126092ce844e215c341078f3770fd90a
Brooke Mullen <Brookemullen@tests-mbp-2.loft.com>
2017-08-01 17:46:09 +0530

    Added buckets for pickup long and lat and plotted

--- initialprocessing.R
+++ initialprocessing.R
@@ -2,7 +2,7 @@
 # ---------------
 # initialize and load libraries
 # ---------------
-rm(list = ls())
+# rm(list = ls())
 library(data.table)
 # # ---------------
 # # reading file
@@ -21,8 +21,8 @@ library(data.table)
 # ---------------
 # Load files afresh
 # ---------------
-load(file = "rawdata/taxi_train.bin")
-load(file = "rawdata/taxi_test.bin")
+load(file = "~/Desktop/datascienceprojects/taxi_project/rawdata/taxi_train.bin")
+load(file = "~/Desktop/datascienceprojects/taxi_project/rawdata/taxi_test.bin")
 
 # ---------------
 # View summary
@@ -288,4 +288,127 @@ dhbuckets <- as.data.frame(taxi_train[,
                                      ,
                                     by = list("hourbuckets"= hour_of_day,"distancebuckets"= distance_buckets)])
 dhbuckets$buckets <- paste0("distance_",as.character(dhbuckets$buckets))
+
+# -----------------------
+# Plotting with map
+# -----------------------
+
+library(ggplot2)
+library(ggmap)
+
+lon <- c(taxi_train$pickup_longitude[1:10000])
+lat <- c(taxi_train$pickup_latitude[1:10000])
+df <- as.data.frame(cbind(lon,lat))
+
+mapgilbert <- get_map(location = c(lon = mean(df$lon), lat = mean(df$lat)), zoom = 12,
+                      maptype = "satellite", scale = 2)
+
+ggmap(mapgilbert) +
+  geom_point(data = df, aes(x = lon, y = lat, fill = "red", alpha = 0.8), size = 2, shape = 21) +
+  guides(fill=FALSE, alpha=FALSE, size=FALSE)
+
+# -----------------
+# plotting on x y 
+# -----------------
+
+plot(taxi_train$pickup_longitude[1:1000])
+plot(taxi_train$pickup_latitude[1:1000])
+
+# states that the most common place is midtown
+
+#----------------------
+# Creating long / lat pick up buckets
+#----------------------
+# http://www.bdcc.co.uk/Gmaps/ll_grat_v3_demo.htm
+
+# top left: (40.85000, -74.03333)
+# bottom left: (40.70000, -74.03333)
+# top right: (40.85000, -73.93333)
+# bottom right: (40.70000, -73.93333)
+
+
+taxi_train$pickup_buckets <- 0
+
+#row 1
+
+taxi_train[(taxi_train$pickup_longitude < -74.01663 & taxi_train$pickup_longitude >= -74.0333) & (taxi_train$pickup_latitude < 40.850 & taxi_train$pickup_latitude >= 40.825),"pickup_buckets" ] <- 1
+taxi_train[(taxi_train$pickup_longitude < -73.99999 & taxi_train$pickup_longitude >= -74.01663) & (taxi_train$pickup_latitude < 40.850 & taxi_train$pickup_latitude >= 40.825),"pickup_buckets"] <- 2
+taxi_train[(taxi_train$pickup_longitude < -73.98333 & taxi_train$pickup_longitude >= -73.99999) & (taxi_train$pickup_latitude < 40.850 & taxi_train$pickup_latitude >= 40.825),"pickup_buckets"] <- 3
+taxi_train[(taxi_train$pickup_longitude < -73.96663 & taxi_train$pickup_longitude >= -73.98333) & (taxi_train$pickup_latitude < 40.850 & taxi_train$pickup_latitude >= 40.825),"pickup_buckets"] <- 4
+taxi_train[(taxi_train$pickup_longitude < -73.94999 & taxi_train$pickup_longitude >= -73.96663) & (taxi_train$pickup_latitude < 40.850 & taxi_train$pickup_latitude >= 40.825),"pickup_buckets"] <- 5
+taxi_train[(taxi_train$pickup_longitude < -73.93333 & taxi_train$pickup_longitude >= -73.94999) & (taxi_train$pickup_latitude < 40.850 & taxi_train$pickup_latitude >= 40.825),"pickup_buckets"] <- 6
+
+# row 2
+  
+taxi_train[(taxi_train$pickup_longitude < -74.01663 & taxi_train$pickup_longitude >= -74.03333) & (taxi_train$pickup_latitude < 40.825 & taxi_train$pickup_latitude >= 40.800),"pickup_buckets"] <- 7
+taxi_train[(taxi_train$pickup_longitude < -73.99999 & taxi_train$pickup_longitude >= -74.01663) & (taxi_train$pickup_latitude < 40.825 & taxi_train$pickup_latitude >= 40.800),"pickup_buckets"] <- 8
+taxi_train[(taxi_train$pickup_longitude < -73.98333 & taxi_train$pickup_longitude >= -73.99999) & (taxi_train$pickup_latitude < 40.825 & taxi_train$pickup_latitude >= 40.800),"pickup_buckets"] <- 9
+taxi_train[(taxi_train$pickup_longitude < -73.96663 & taxi_train$pickup_longitude >= -73.98333) & (taxi_train$pickup_latitude < 40.825 & taxi_train$pickup_latitude >= 40.800),"pickup_buckets"] <- 10
+taxi_train[(taxi_train$pickup_longitude < -73.94999 & taxi_train$pickup_longitude >= -73.96663) & (taxi_train$pickup_latitude < 40.825 & taxi_train$pickup_latitude >= 40.800),"pickup_buckets"] <- 11
+taxi_train[(taxi_train$pickup_longitude < -73.93333 & taxi_train$pickup_longitude >= -73.94999) & (taxi_train$pickup_latitude < 40.825 & taxi_train$pickup_latitude >= 40.800),"pickup_buckets"] <- 12
+
+#row 3
+  
+taxi_train[(taxi_train$pickup_longitude < -74.01663 & taxi_train$pickup_longitude >= -74.03333) & (taxi_train$pickup_latitude < 40.800 & taxi_train$pickup_latitude >= 40.775),"pickup_buckets"] <- 13
+taxi_train[(taxi_train$pickup_longitude < -73.99999 & taxi_train$pickup_longitude >= -74.01663) & (taxi_train$pickup_latitude < 40.800 & taxi_train$pickup_latitude >= 40.775),"pickup_buckets"] <- 14
+taxi_train[(taxi_train$pickup_longitude < -73.98333 & taxi_train$pickup_longitude >= -73.99999) & (taxi_train$pickup_latitude < 40.800 & taxi_train$pickup_latitude >= 40.775),"pickup_buckets"] <- 15
+taxi_train[(taxi_train$pickup_longitude < -73.96663 & taxi_train$pickup_longitude >= -73.98333) & (taxi_train$pickup_latitude < 40.800 & taxi_train$pickup_latitude >= 40.775),"pickup_buckets"] <- 16
+taxi_train[(taxi_train$pickup_longitude < -73.94999 & taxi_train$pickup_longitude >= -73.96663) & (taxi_train$pickup_latitude < 40.800 & taxi_train$pickup_latitude >= 40.775),"pickup_buckets"] <- 17
+taxi_train[(taxi_train$pickup_longitude < -73.93333 & taxi_train$pickup_longitude >= -73.94999) & (taxi_train$pickup_latitude < 40.800 & taxi_train$pickup_latitude >= 40.775),"pickup_buckets"] <- 18
+#row 4
+  
+taxi_train[(taxi_train$pickup_longitude < -74.01663 & taxi_train$pickup_longitude >= -74.03333) & (taxi_train$pickup_latitude < 40.775 & taxi_train$pickup_latitude >= 40.750),"pickup_buckets"] <- 19
+taxi_train[(taxi_train$pickup_longitude < -73.99999 & taxi_train$pickup_longitude >= -74.01663) & (taxi_train$pickup_latitude < 40.775 & taxi_train$pickup_latitude >= 40.750),"pickup_buckets"] <- 20
+taxi_train[(taxi_train$pickup_longitude < -73.98333 & taxi_train$pickup_longitude >= -73.99999) & (taxi_train$pickup_latitude < 40.775 & taxi_train$pickup_latitude >= 40.750),"pickup_buckets"] <- 21
+taxi_train[(taxi_train$pickup_longitude < -73.96663 & taxi_train$pickup_longitude >= -73.98333) & (taxi_train$pickup_latitude < 40.775 & taxi_train$pickup_latitude >= 40.750),"pickup_buckets"] <- 22
+taxi_train[(taxi_train$pickup_longitude < -73.94999 & taxi_train$pickup_longitude >= -73.96663) & (taxi_train$pickup_latitude < 40.775 & taxi_train$pickup_latitude >= 40.750),"pickup_buckets"] <- 23
+taxi_train[(taxi_train$pickup_longitude < -73.93333 & taxi_train$pickup_longitude >= -73.94999) & (taxi_train$pickup_latitude < 40.775 & taxi_train$pickup_latitude >= 40.750),"pickup_buckets"] <- 24
+# row 5
+  
+taxi_train[(taxi_train$pickup_longitude < -74.01663 & taxi_train$pickup_longitude >= -74.03333) & (taxi_train$pickup_latitude < 40.750 & taxi_train$pickup_latitude >= 40.725),"pickup_buckets"] <- 25
+taxi_train[(taxi_train$pickup_longitude < -73.99999 & taxi_train$pickup_longitude >= -74.01663) & (taxi_train$pickup_latitude < 40.750 & taxi_train$pickup_latitude >= 40.725),"pickup_buckets"] <- 26
+taxi_train[(taxi_train$pickup_longitude < -73.98333 & taxi_train$pickup_longitude >= -73.99999) & (taxi_train$pickup_latitude < 40.750 & taxi_train$pickup_latitude >= 40.725),"pickup_buckets"] <- 27
+taxi_train[(taxi_train$pickup_longitude < -73.96663 & taxi_train$pickup_longitude >= -73.98333) & (taxi_train$pickup_latitude < 40.750 & taxi_train$pickup_latitude >= 40.725),"pickup_buckets"] <- 28
+taxi_train[(taxi_train$pickup_longitude < -73.94999 & taxi_train$pickup_longitude >= -73.96663) & (taxi_train$pickup_latitude < 40.750 & taxi_train$pickup_latitude >= 40.725),"pickup_buckets"] <- 29
+taxi_train[(taxi_train$pickup_longitude < -73.93333 & taxi_train$pickup_longitude >= -73.94999) & (taxi_train$pickup_latitude < 40.750 & taxi_train$pickup_latitude >= 40.725),"pickup_buckets"] <- 30
+# row 6
+  
+taxi_train[(taxi_train$pickup_longitude < -74.01663 & taxi_train$pickup_longitude >= -74.03333) & (taxi_train$pickup_latitude < 40.725 & taxi_train$pickup_latitude >= 40.700),"pickup_buckets"] <- 31
+taxi_train[(taxi_train$pickup_longitude < -73.99999 & taxi_train$pickup_longitude >= -74.01663) & (taxi_train$pickup_latitude < 40.725 & taxi_train$pickup_latitude >= 40.700),"pickup_buckets"] <- 32
+taxi_train[(taxi_train$pickup_longitude < -73.98333 & taxi_train$pickup_longitude >= -73.99999) & (taxi_train$pickup_latitude < 40.725 & taxi_train$pickup_latitude >= 40.700),"pickup_buckets"] <- 33 
+taxi_train[(taxi_train$pickup_longitude < -73.96663 & taxi_train$pickup_longitude >= -73.98333) & (taxi_train$pickup_latitude < 40.725 & taxi_train$pickup_latitude >= 40.700),"pickup_buckets"] <- 34
+taxi_train[(taxi_train$pickup_longitude < -73.94999 & taxi_train$pickup_longitude >= -73.96663) & (taxi_train$pickup_latitude < 40.725 & taxi_train$pickup_latitude >= 40.700),"pickup_buckets"] <- 35
+taxi_train[(taxi_train$pickup_longitude < -73.93333 & taxi_train$pickup_longitude >= -73.94999) & (taxi_train$pickup_latitude < 40.725 & taxi_train$pickup_latitude >= 40.700),"pickup_buckets"] <- 36
+
+# here it is for latitude coordinates
+# taxi_train[taxi_train$pickup_latitude < 40.850 & taxi_train$pickup_latitude >= 40.825,"pickup_buckets"]
+# taxi_train[taxi_train$pickup_latitude < 40.825 & taxi_train$pickup_latitude >= 40.800,"pickup_buckets"]
+# taxi_train[taxi_train$pickup_latitude < 40.800 & taxi_train$pickup_latitude >= 40.775,"pickup_buckets"]
+# taxi_train[taxi_train$pickup_latitude < 40.775 & taxi_train$pickup_latitude >= 40.750,"pickup_buckets"]
+# taxi_train[taxi_train$pickup_latitude < 40.750 & taxi_train$pickup_latitude >= 40.725,"pickup_buckets"]
+# taxi_train[taxi_train$pickup_latitude < 40.725 & taxi_train$pickup_latitude >= 40.700,"pickup_buckets"]
+
+
+pubuckets <- as.data.frame(taxi_train[,
+                                      {list(
+                                        "num_in_bucket" = .N,
+                                        "min_speed" = min(speed),
+                                        "max_speed" = max(speed),
+                                        "avg_speed" = mean(speed),
+                                        "median_speed" = median(speed),
+                                        "p5_speed" = quantile(speed,0.05),
+                                        "p15_speed" = quantile(speed,0.15),
+                                        "p25_speed" = quantile(speed,0.25),
+                                        "p35_speed" = quantile(speed,0.35),
+                                        "p45_speed" = quantile(speed,0.45),
+                                        "p55_speed" = quantile(speed,0.55),
+                                        "p65_speed" = quantile(speed,0.65),
+                                        "p75_speed" = quantile(speed,0.75),
+                                        "p85_speed" = quantile(speed,0.85),
+                                        "p95_speed" = quantile(speed,0.95)
+                                      )
+                                      }
+                                      ,
+                                      by = ("pubuckets"= pickup_buckets)])
+pubuckets$buckets <- paste0("pickup_bucket_",as.character(pubuckets$buckets))
 
